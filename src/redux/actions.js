2161eb99dc25e7dba6f20d74ef86665a8e6187c6
Ismayil <hajiev93@gmail.com>
2020-08-30 17:23:09 +0300

    startet toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,4 @@
+import { createAction } from "@reduxjs/toolkit";
 /* TYPES */
 export const ADDCONTACT = "contact/ADD";
 export const DELETECONTACT = "contact/DEL";
@@ -6,37 +7,14 @@ export const GETFILTERVALUE = "contact/GETFV";
 export const ALERTHANDLER = "contact/ALERT";
 
 /* ACTIONS */
-const onAddContact = (contact) => {
-  return {
-    type: ADDCONTACT,
-    payload: { contact },
-  };
-};
-const onDeleteContact = (id) => {
-  return {
-    type: DELETECONTACT,
-    payload: { id },
-  };
-};
 
-const filterValue = (e) => {
-  return {
-    type: FILTERVALUE,
-    payload: { filter: e.target.value },
-  };
-};
-
-const switchAlert = () => {
-  return {
-    type: ALERTHANDLER,
-  };
-};
-
-// const getFilterData = () => {
-//   return {
-//     type: GETFILTERVALUE,
-//     // payload: { filter: e.target.value },
-//   };
-// };
+const onAddContact = createAction("contact/ADD");
+const onDeleteContact = createAction("contact/DEL");
+const filterValue = createAction("contact/FV", (e) => ({
+  payload: {
+    filter: e.target.value,
+  },
+}));
+const switchAlert = createAction("contact/ALERT");
 
 export default { onAddContact, onDeleteContact, filterValue, switchAlert };
--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -1,33 +1,48 @@
 import { combineReducers } from "redux";
-import {
-  ADDCONTACT,
-  DELETECONTACT,
-  FILTERVALUE,
-  ALERTHANDLER,
-} from "./actions";
+import { createReducer } from "@reduxjs/toolkit";
+import actions from "./actions";
 
-const items = (
-  state = [
+const addTask = (state, action) => {
+  return [...state, action.payload];
+};
+const removeTast = (state, action) => {
+  return state.filter((contact) => contact.id !== action.payload);
+};
+const items = createReducer(
+  [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ],
-  { type, payload }
-) => {
-  switch (type) {
-    case ADDCONTACT:
-      return [...state, payload.contact];
-    case DELETECONTACT:
-      return state.filter((contact) => contact.id !== payload.id);
-
-    default:
-      return state;
+  {
+    [actions.onAddContact]: addTask,
+    [actions.onDeleteContact]: removeTast,
   }
-};
+);
+
+// const items = (
+//   state = [
+//     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//   ],
+//   { type, payload }
+// ) => {
+//   switch (type) {
+//     case actions.onAddContact.type:
+//       return [...state, payload];
+//     case actions.onDeleteContact.type:
+//       return state.filter((contact) => contact.id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
 const filter = (state = "", { type, payload }) => {
   switch (type) {
-    case FILTERVALUE:
+    case actions.filterValue.type:
       return (state = payload.filter);
 
     default:
@@ -36,7 +51,7 @@ const filter = (state = "", { type, payload }) => {
 };
 const alert = (state = false, { type, payload }) => {
   switch (type) {
-    case ALERTHANDLER:
+    case actions.switchAlert.type:
       return (state = !state);
 
     default:
